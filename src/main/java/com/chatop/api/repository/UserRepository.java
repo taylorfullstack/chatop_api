205fb00dfce702b09b6607ee06c9495dbb3ea8bf
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-12-13 15:35:20 +0100

    add User, Rental, and Message repositories

---
+++ src/main/java/com/chatop/api/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.chatop.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.chatop.api.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
+}
