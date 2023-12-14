4607b3c8afb66fe7d03e4fef8db99708fc98dfe8
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2023-12-14 13:58:06 +0100

    update chatop.sql commands to recreate db and add mock data

--- chatop.sql
+++ chatop.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS chatop;
+CREATE DATABASE chatop;
+USE chatop;
+
 CREATE TABLE users (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name varchar(100) NOT NULL,
@@ -19,8 +23,8 @@ CREATE TABLE rentals(
   owner_id BIGINT NOT NULL,
   created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
-
+  PRIMARY KEY (id),
+  FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
 CREATE TABLE messages (
@@ -29,14 +33,20 @@ CREATE TABLE messages (
     user_id BIGINT NOT NULL,
     message varchar(2000),
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (rental_id) REFERENCES rentals(id)
 );
 
 CREATE UNIQUE INDEX users_index ON users (email);
 
-ALTER TABLE users ADD FOREIGN KEY (id) REFERENCES rentals (owner_id);
-ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users (id);
-ALTER TABLE messages ADD FOREIGN KEY (rental_id) REFERENCES rentals (id);
 
+INSERT INTO users (name, email, password) VALUES
+('Test User', 'test@test.com', SHA2('password', 256)),
+('Heather Taylor', 'heather@test.com', SHA2('password', 256));
 
+INSERT INTO rentals (name, surface, price, picture, description, owner_id) VALUES
+('Lakefront Cottage', 100, 150, 'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_1280.jpg', 'Quiet cottage in the mountains', 1),
+('Beach House', 200, 250, 'https://cdn.pixabay.com/photo/2020/10/13/13/28/ameland-5651866_1280.jpg', 'Beautiful beach house', 2),
+('Snowy Cabin', 100, 399,  'https://cdn.pixabay.com/photo/2016/11/19/14/30/aurora-borealis-1839582_1280.jpg', 'Gorgeous view of the aurora borealis', 2);
 
